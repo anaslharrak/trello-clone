d86a4181f1ae31c75820614a118c8c333ea4dee0
Anas Lharrak <146816560+anaslharrak@users.noreply.github.com>
2024-06-18 20:32:21 +0200

    fix: :zap: fix display of the tasks
    fixed the display of the tasks (TODO: styling of the tasks, and the functionallity of edit the information)

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,6 +7,19 @@ const Card = ({column}: {column: Column}) => {
     return (
         <StyledCard>
             <h1>{column.title}</h1>
+
+            <div>
+                {
+                    column.tasks.map((task) => {
+                        return (
+                            <div key={task.id}>
+                                <h3>{task.title}</h3>
+                                <p>{task.description}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </StyledCard>
     )
 }
